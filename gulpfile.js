b10cf085028b18e2c812d8e60fec156cc110e4da
Cheryl FitzGerald <cherylafitzgerald@gmail.com>
2020-05-11 15:51:18 -0700

    wip"

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,14 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
+var count = require('gulp-count');
 
 gulp.task('concat-ais-css', function() {
   console.log('test');
 });
 
 gulp.task('concat-aisforms-css', function() {
-  return gulp.src('./app/aisforms/styles/aisforms-common/*.css')
+  return gulp.src('./assets/aisforms/styles/shared/styles/*.css', './assets/aisforms/styles/aisforms-common/aisformsCommon.css')
+  	.pipe(count('## css-files selected'))
     .pipe(concat('aisformsCommon.css'))
-    .pipe(gulp.dest('./dist/aisforms/styles/'));
+    .pipe(gulp.dest('./static/aisforms/styles/'));
 });
