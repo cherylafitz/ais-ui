409ceb03543a9d1f6ade859ede39ea1139b16ba2
Cheryl FitzGerald <cherylafitz@users.noreply.github.com>
2020-05-18 20:24:43 -0700

    Ais details default (#196)
    * WIP
    
    * WIP
    
    * wip"
    
    * Updated gulp to concat shared files
    
    * wip
    
    * WIP
    
    * WIP
    
    * re-added base styles to remove change from branch
    
    * WIP

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,16 @@
-var gulp = require('gulp');
-var concat = require('gulp-concat');
+
+var gulp = require('gulp'),
+	concat = require('gulp-concat'),
+	watch = require('gulp-watch');
 
 gulp.task('concat-ais-css', function() {
-  console.log('test');
+  return gulp.src(['./assets/shared/styles/*.css','./assets/ais/styles/aisCommon.css'])
+    .pipe(concat('aisCommon.css'))
+    .pipe(gulp.dest('./static/styles/'));
 });
 
 gulp.task('concat-aisforms-css', function() {
-  return gulp.src('./app/aisforms/styles/aisforms-common/*.css')
+  return gulp.src(['./assets/shared/styles/*.css','./assets/aisforms/styles/aisforms-common/aisformsCommon.css'])
     .pipe(concat('aisformsCommon.css'))
-    .pipe(gulp.dest('./dist/aisforms/styles/'));
+    .pipe(gulp.dest('./static/styles/'));
 });
