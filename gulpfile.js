6909d7992bb37e1156e5b3539ed8be739a7b249b
Cheryl FitzGerald <cherylafitzgerald@gmail.com>
2020-06-18 19:44:42 -0700

    Alphebatized inline question selectors, made shared folder for print and standard common files to pull in inline Qs

--- gulpfile.js
+++ gulpfile.js
@@ -16,13 +16,13 @@ gulp.task('concat-aisforms-css', function() {
 });
 
 gulp.task('concat-standardcommon-css', function() {
-  return gulp.src('./assets/aisforms/styles/standard-common/*.css') // conly combines files in standard-common folder, not shared
+  return gulp.src('./assets/aisforms/styles/standard-common/*.css','./assets/aisforms/styles/shared-forms/*.css') // conly combines files in standard-common folder and shared-forms, not shared
     .pipe(concat('standardCommon.css'))
     .pipe(gulp.dest('./static/styles/'));
 });
 
 gulp.task('concat-printcommon-css', function() {
-  return gulp.src('./assets/aisforms/styles/print-common/*.css') // conly combines files in print-common folder, not shared
+  return gulp.src('./assets/aisforms/styles/print-common/*.css','./assets/aisforms/styles/shared-forms/*.css') // conly combines files in print-common folder and shared-forms, not shared
     .pipe(concat('printCommon.css'))
     .pipe(gulp.dest('./static/styles/'));
 });
